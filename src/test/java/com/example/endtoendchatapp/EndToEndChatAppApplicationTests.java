154a8e2f211fd6925c19c77312d39d2d925e21ce
Zigix <olekwszk7@gmail.com>
2023-08-12 18:35:23 +0200

    Initial commit

---
+++ src/test/java/com/example/endtoendchatapp/EndToEndChatAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.endtoendchatapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EndToEndChatAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
